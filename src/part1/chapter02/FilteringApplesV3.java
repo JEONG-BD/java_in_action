bf6cd60d9bb0bf9c2b043e13faebb5e0b44c2fe0
JEONG-BD <dev.byeongdu@gmail.com>
2025-04-24 16:02:21 +0900

    compare

---
+++ src/part1/chapter02/FilteringApplesV3.java
@@ -0,0 +1,57 @@
+package part1.chapter02;
+import static part1.Color.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import part1.Apple;
+import part1.Color;
+
+public class FilteringApplesV3 {
+    public static void main(String[] args) {
+        List<Apple>inventory = Arrays.asList(
+            new Apple(80, Color.GREEN),
+            new Apple(155, Color.GREEN),
+            new Apple(120, Color.RED));        
+            
+            System.out.println(inventory);
+            inventory.sort(new Comparator<Apple>() {
+                public int compare(Apple a1, Apple a2){
+                    return Integer.compare(a1.getWeight(), a2.getWeight());
+                }
+            });
+
+            System.out.println(inventory);
+        }
+
+ 
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate){
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if(predicate.test(apple)){
+                result.add(apple);
+            }
+        }
+        
+        return result;
+    }
+
+    public static <T> List<T> filter(List<T> inventory, Predicate<T> predicate){
+        List<T> result = new ArrayList<>();
+
+        for (T apple : inventory) {
+            if(predicate.test(apple)){
+                result.add(apple);
+            }
+        }
+        
+        return result;
+    }
+
+
+
+}
