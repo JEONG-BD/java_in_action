14bc6c6dc58afeb2b5c6878909b46ff788b8be4d
JEONG-BD <dev.byeongdu@gmail.com>
2025-06-12 20:26:24 +0900

    async API

---
+++ src/main/java/part5/chapter16/ShopMain.java
@@ -0,0 +1,29 @@
+package part5.chapter16;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class ShopMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ShopV1 shopV1 = new ShopV1("Test");
+        long startTime = System.currentTimeMillis();
+        //double resultValue = shopV1.getPrices("test");
+        Future<Double> result = shopV1.getAsyncPrices("test");
+
+        doSomethingElse();
+        try {
+            Double resultValue = result.get();
+            System.out.printf("Price us % .2fn", resultValue);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        long endTime = System.currentTimeMillis();
+
+        //System.out.printf("Price us % .2fn", resultValue);
+
+    }
+
+    public static void doSomethingElse() {
+        System.out.println("5. [doSomethingElse method]");
+    }
+}
--- src/main/java/part5/chapter16/ShopV1.java
+++ src/main/java/part5/chapter16/ShopV1.java
@@ -1,6 +1,8 @@
 package part5.chapter16;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 public class ShopV1 {
 
@@ -14,17 +16,32 @@ public class ShopV1 {
 
     // 가격을 반환한다.
     public double getPrices(String product){
+        System.out.println("1. [getPrices method]");
+
         return calculatePrice(product);
     }
 
+    public Future<Double> getAsyncPrices(String product){
+        System.out.println("1. [getPrices method]");
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            double price = calculatePrice(product);
+            futurePrice.complete(price);
+        }).start();
+        return futurePrice;
+    }
+
     public double calculatePrice(String product) {
+        System.out.println("2. [calculatePrice method]");
         delay();
         return random.nextDouble() * product.charAt(0) + product.charAt(1);
     }
 
     public static void delay(){
         try {
-            Thread.sleep(1000L);
+            System.out.println("3. [delay method Start]");
+            Thread.sleep(8000L);
+            System.out.println("4. [delay method End]");
         }catch (InterruptedException ie){
             throw new RuntimeException(ie);
         }
